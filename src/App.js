50d42d18262dd1ac2d656b3c9a7338f19b3ecf3a
claudiahalip <claudia4ro@yahoo.com>
2021-03-19 13:01:28 -0500

    home page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
-      <h1>Netflix-clone</h1>
+      <Home/>
     </div>
   );
 }
---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import img1 from '../images/netflix-screenshot.png';
+
+
+export default function Home() {
+    return (
+        <div>
+            <h1>Netflix-clone</h1>
+            <img src={img1} alt=""/>
+        </div>
+    )
+}
