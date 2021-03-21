ef6d776b3ea021536be9f8b497a0e702ca603790
claudiahalip <claudia4ro@yahoo.com>
2021-03-20 19:09:39 -0500

    API added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import img1 from '../images/netflix-screenshot.png';
 
 
 export default function Home() {
+   
+    const API_URL = `https://api.themoviedb.org/3/movie/550?api_key=${process.env.REACT_APP_API_KEY }`
+
+    const [data, setData] = useState({});
+    // const [error, setError] = useState(null);
+    // const [isLoaded, setIsLoaded] = useState(false);
+   
+    useEffect(() => {
+        fetch(API_URL)
+
+        // fetch(`https://api.themoviedb.org/3/movie/550?api_key=${API_KEY}`)
+        
+        .then(res => res.json())
+        .then( (data) =>  {
+            setData(data)
+            }
+        )
+    }, [])
+
     return (
         <div>
             <h1>Netflix-clone</h1>
@@ -10,3 +29,5 @@ export default function Home() {
         </div>
     )
 }
+
+
