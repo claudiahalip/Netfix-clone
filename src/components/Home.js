9f9900afe1afb749620074118c7cd578fb21d86b
claudiahalip <claudia4ro@yahoo.com>
2021-03-24 22:52:25 -0500

    css ad routes

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,32 +1,31 @@
 import React, {useState, useEffect} from 'react';
 import img1 from '../images/netflix-img.jpg';
-import Movie from '../components/Movie'
+import Movie from '../components/Movie';
+import '../css/Home.css';
+import {Route, Switch} from 'react-router-dom';
 
 export default function Home() {
-   
-    
+
     const API_URL=`https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_API_KEY }&sort_by=revenue.desc`
 
-    const [data, setData] = useState({});
+    const [data, setData] = useState([]);
     // const [error, setError] = useState(null);
     // const [isLoaded, setIsLoaded] = useState(false);
    
     useEffect(() => {
         fetch(API_URL)
-
-        // fetch(`https://api.themoviedb.org/3/movie/550?api_key=${API_KEY}`)
-        
         .then(res => res.json())
         .then( (data) =>  {
-           setData([data.results])
+           setData([...data.results])
             }
         )
     },[])
-
+    
+    //console.log(data[0].title)
     
     console.log(data)
-    // let movies = data.results;
-    //backdrop_path, original_title, overview, poster_path, release_date,
+    // // let movies = data.results;
+    // //backdrop_path, original_title, overview, poster_path, release_date,
     const moviesList = data.map( ({backdrop_path, original_title, overview, poster_path, release_date,id}) => ( 
        <li key={id}>
            <Movie
@@ -41,10 +40,13 @@ export default function Home() {
     
     return (
         <div>
+            <Route exact path='/'>
             <h1>Netflix-clone</h1>
             <img src={img1} alt=""/>
-           <ul >{moviesList}</ul>
-           
+            </Route>
+            <Route exact path='/movies'>
+            <ul className="allMovies"><h1>Films with the Highest Revenue</h1>{moviesList}</ul>
+            </Route>
         </div>
     )
 }
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import defaultImage from '../images/netflix-img.jpg';
+import '../css/Movie.css';
+
 
 export default function Movie({backdrop_path, original_title, overview, poster_path, release_date}) {
     return (
+        
         <div className="card">
-         <img src={backdrop_path} alt=""/>
-         <h1 >Movie Test</h1>  
-         <h1>{original_title}</h1>          
+            
+            <img src = {`http://image.tmdb.org/t/p/w500/${backdrop_path}`} alt={defaultImage}/>
+            <h1>{original_title}</h1> 
+            <h3>Release date: {release_date}</h3> 
+            <p>{overview}</p>
+           
         </div>
+       
+       
     )
 }
